0036a921f7f95c75350b3cb2ba3049dddb805782
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-05-17 00:55:04 -0500

    clase user

---
+++ app/src/main/java/com/example/applab4alfonsoalvarado_shantaldeleon/user.java
@@ -0,0 +1,18 @@
+package com.example.applab4alfonsoalvarado_shantaldeleon;
+
+public class user {
+
+    public String email;
+    public String pass;
+    public String nombre;
+    public String cedula;
+    public String rol;
+
+    public user(String email, String pass, String nombre, String cedula, String rol){
+        this.email = email;
+        this.pass = pass;
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.rol = rol;
+    }
+}
