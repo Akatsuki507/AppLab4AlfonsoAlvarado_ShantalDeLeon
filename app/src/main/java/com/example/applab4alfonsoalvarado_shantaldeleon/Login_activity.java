d217a5da40795e9863754194b652f02fdaec6023
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-05-17 02:50:31 -0500

    TIENE VIDA CHUCHAMADRE

--- app/src/main/java/com/example/applab4alfonsoalvarado_shantaldeleon/Login_activity.java
+++ app/src/main/java/com/example/applab4alfonsoalvarado_shantaldeleon/Login_activity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 
 public class Login_activity extends AppCompatActivity {
-
+    ArrayList<user> users;
     private EditText User,pass;
     private Button Btn,Btn2;
 
@@ -21,7 +29,7 @@ public class Login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
-
+        loadData();
         User = (EditText)findViewById(R.id.Usuario);
         pass = (EditText)findViewById(R.id.Contraseña);
         Btn = (Button)findViewById(R.id.Boton);
@@ -32,7 +40,7 @@ public class Login_activity extends AppCompatActivity {
             public void onClick(View v) {
                 String USR = User.getText().toString();
                 String PASS = pass.getText().toString();
-                if (USR.equals("nombre@utp.ac.pa") && PASS.equals("12345")){
+                if (verificar(USR, PASS)){
 
                     Intent i = new Intent(getApplicationContext(),welcome.class);
                     startActivity(i);
@@ -51,5 +59,29 @@ public class Login_activity extends AppCompatActivity {
             }
         });
 
+    }
+    public void loadData(){
+        SharedPreferences sharepreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharepreferences.getString("usuarios", null);
+        Type type = new TypeToken<ArrayList<user>>(){}.getType();
+        users = gson.fromJson(json, type);
+
+        if(users == null){
+            users = new ArrayList<>();
+        }
+        for (int counter = 0; counter < users.size(); counter++) {
+            Log.i("USUARIO", users.get(counter).email);
+        }
+    }
+
+    private boolean verificar(String email, String pass){
+        for (int counter = 0; counter < users.size(); counter++) {
+            if((users.get(counter).email.equals(email)) && (users.get(counter).pass.equals(pass))){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- app/src/main/java/com/example/applab4alfonsoalvarado_shantaldeleon/sign_in.java
+++ app/src/main/java/com/example/applab4alfonsoalvarado_shantaldeleon/sign_in.java
@@ -3,21 +3,40 @@ package com.example.applab4alfonsoalvarado_shantaldeleon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Console;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 public class sign_in extends AppCompatActivity {
-
+    ArrayList<user> users;
     EditText nombre, cedula ,correo, contraseña;
+    Button Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Btn = (Button)findViewById(R.id.Boton);
+        Btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Registro();
+            }
+        });
         this.Inicializar();
     }
     private void Inicializar(){
@@ -27,11 +46,49 @@ public class sign_in extends AppCompatActivity {
         contraseña=(EditText)findViewById(R.id.contraseña_registro);
     }
 
-    public void Registro(View view) {
+    public void Registro() {
         if(Validar()) {
+            users = new ArrayList<>();
+
+            String Nombre = nombre.getText().toString();
+            String Cedula = cedula.getText().toString();
+            String Correo = correo.getText().toString();
+            String Contraseña = contraseña.getText().toString();
+
+            loadData();
+            user nuevo_usuario = new user(Correo,Contraseña,Nombre,Cedula, "usuario");
+            users.add(nuevo_usuario);
+            saveData();
             Toast.makeText(this, "Registro Completo", Toast.LENGTH_SHORT).show();
             Intent login = new Intent(this, Login_activity.class);
             startActivity(login);
+        }
+    }
+
+    private void saveData(){
+        SharedPreferences sharepreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharepreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(users);
+        editor.putString("usuarios", json);
+        editor.apply();
+        loadData();
+
+        Log.i("ADVERTENCIA", users.get(0).email);
+    }
+
+    public void loadData(){
+        SharedPreferences sharepreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharepreferences.getString("usuarios", null);
+        Type type = new TypeToken<ArrayList<user>>(){}.getType();
+        users = gson.fromJson(json, type);
+
+        if(users == null){
+            users = new ArrayList<>();
+        }
+        for (int counter = 0; counter < users.size(); counter++) {
+            Log.i("USUARIO", users.get(counter).email);
         }
     }
 
